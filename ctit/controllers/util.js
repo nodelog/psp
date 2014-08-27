77ecfa7a614ca1e4996f3f9d5c22364d7004f448
wyc0991 <wangyachao0991@sina.cn>
2014-08-27 17:02:57 +0800

    remove no use project

--- ctit/controllers/util.js
+++
@@ -1,6 +0,0 @@
-exports.trim = function (str) {
-    return str.replace(/(^\s*)|(\s*$)/g, "");
-}
-String.prototype.trim = function () {
-    return this.replace(/(^\s*)|(\s*$)/g, "");
-}
--- ctit/models/mongodb.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require('mongoose');
-var settings = require('../settings');
-mongoose.connect(settings.url);
-exports.mongoose = mongoose;
